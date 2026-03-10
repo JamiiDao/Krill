0306e505d9ef39f24df0571c1b700c00d9bd100a
448 OG <superuser@448.africa>
2026-03-10 15:37:26 +0300

    Add the commandline configuration for the server passcode
    - Now a random 8 character passcode is printed in the console with further
      instructions on starting the server.
    
    - This random passcode is valid for 30 minutes.
    
    - The passcode is ephemeral on each server restart
    
    - New random character generation has been added to the common utilities
    
    - A monotonic TAI64N timestamp is used to check if the code has expired

--- krill-common/src/types.rs
+++ krill-common/src/types.rs
@@ -1,3 +1,16 @@
+#[cfg(feature = "random")]
+use core::fmt;
+#[cfg(feature = "random")]
+use std::time::Duration;
+
+#[cfg(feature = "random")]
+use tai64::Tai64N;
+#[cfg(feature = "random")]
+use zeroize::Zeroizing;
+
+#[cfg(feature = "random")]
+use crate::RandomChars;
+
 use bitcode::{Decode, Encode};
 
 pub type Message32ByteHash = [u8; 32];
@@ -14,5 +27,78 @@ impl UserRole {
             Self::Administrator => "administrator",
             Self::Member => "member",
         }
+    }
+}
+
+#[cfg(feature = "random")]
+pub struct AdminConfiguration {
+    timestamp: Tai64N,
+    secret: Option<RandomChars<8>>,
+}
+
+#[cfg(feature = "random")]
+impl AdminConfiguration {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn timestamp(&self) -> &Tai64N {
+        &self.timestamp
+    }
+
+    pub fn is_expired_after_30(&self) -> bool {
+        self.is_expired(Duration::from_mins(30))
+    }
+
+    pub fn is_expired(&self, duration: Duration) -> bool {
+        let now = Tai64N::now();
+
+        now.duration_since(&self.timestamp)
+            .is_ok_and(|value| value > duration)
+    }
+
+    pub fn secret(&self) -> Option<&RandomChars<8>> {
+        self.secret.as_ref()
+    }
+
+    pub fn to_string(&self) -> Option<Zeroizing<String>> {
+        self.secret.as_ref().map(|secret_chars| {
+            let mut outcome = Zeroizing::new(String::with_capacity(8));
+            secret_chars
+                .expose()
+                .iter()
+                .for_each(|char| outcome.push(*char));
+
+            outcome
+        })
+    }
+
+    pub fn const_cmp(&self, other: &str) -> bool {
+        if let Some(secret) = self.secret.as_ref() {
+            secret.const_cmp(other)
+        } else {
+            false
+        }
+    }
+
+    pub fn clear(&mut self) {
+        drop(self.secret.take());
+    }
+}
+
+#[cfg(feature = "random")]
+impl Default for AdminConfiguration {
+    fn default() -> Self {
+        Self {
+            timestamp: Tai64N::now(),
+            secret: Some(RandomChars::generate()),
+        }
+    }
+}
+
+#[cfg(feature = "random")]
+impl fmt::Debug for AdminConfiguration {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "AdminConfiguration(Redacted 8 characters)",)
     }
 }
